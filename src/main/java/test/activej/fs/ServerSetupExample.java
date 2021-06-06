855aaa8bd6f1dfe95a6beee721cf78c4e632c246
arsentii.kaptsan <arsentii.kaptsan@gmail.com>
2021-06-06 03:37:05 +0300

    Init commit

---
+++ src/main/java/test/activej/fs/ServerSetupExample.java
@@ -0,0 +1,40 @@
+package test.activej.fs;
+
+import io.activej.config.Config;
+import io.activej.inject.Injector;
+import io.activej.launcher.Launcher;
+import io.activej.launchers.fs.SimpleTcpServerLauncher;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * This example demonstrates configuring and launching ActiveFsServer.
+ */
+//[START EXAMPLE]
+public class ServerSetupExample extends SimpleTcpServerLauncher {
+	private Path storage;
+
+	@Override
+	protected void onInit(Injector injector) throws Exception {
+		storage = Files.createTempDirectory("server_storage");
+	}
+
+	@Override
+	protected Config createConfig() {
+		return super.createConfig()
+				.with("activefs.path", storage.toString())
+				.with("activefs.listenAddresses", "*:6732");
+	}
+
+	@Override
+	protected void run() throws Exception {
+		awaitShutdown();
+	}
+
+	public static void main(String[] args) throws Exception {
+		Launcher launcher = new ServerSetupExample();
+		launcher.launch(args);
+	}
+}
+//[END EXAMPLE]
